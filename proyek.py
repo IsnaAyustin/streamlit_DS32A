2c29f0768827c37eb4cf6ad7e46b14dfeda9ccf7
Isna <isnaayustin@gmail.com>
2025-07-08 23:47:14 +0700

    initial upload

--- proyek.py
+++ proyek.py
@@ -1,11 +1,48 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def tampilkan():
-    data = pd.DataFrame(np.random.randn(50, 3), columns=['A', 'B', 'C'])
-    st.line_chart(data)
+    # 1. Load data
+    def load_data():
+        df = pd.read_csv('bank_churn_data.csv', sep='\t')
+        return df 
 
-    st.markdown("### Filter Data")
-    range_slider = st.slider("Pilih range nilai:", 0, 100, (25, 75))
-    st.write(f"Anda memilih range: {range_slider}")
+    df = load_data() 
+
+    # 2. Show preview
+    st.subheader("Preview Data")
+    st.dataframe(df.head())
+
+tampilkan()
+
+# 2. Preprocessing sederhana
+st.subheader("💡 Preprocessing dan Pelatihan Model Bank (Churn)")
+st.subheader("📊 Visualisasi EDA Interaktif")
+
+tab1, tab2, = st.tabs(["Distribusi Usia", "Gender & Level Edukasi"])
+
+with tab1:
+    st.markdown("### Usia vs Churn")
+    fig, ax = plt.subplots()
+    df_plot = pd.read_csv("bank_churn_data.csv")
+    sns.histplot(data=df_plot, x='customer_age', hue='attrition_flag', kde=True, bins=30, palette='coolwarm', ax=ax)
+    st.pyplot(fig)
+
+with tab2:
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("### Churn berdasarkan Gender")
+        fig, ax = plt.subplots()
+        sns.countplot(data=df_plot, x='gender', hue='attrition_flag', palette='Set2', ax=ax)
+        st.pyplot(fig)
+
+    with col2:
+        st.markdown("### Churn berdasarkan Level Edukasi")
+        fig, ax = plt.subplots()
+        sns.countplot(data=df_plot, x='education_level', hue='attrition_flag', palette='Set1', ax=ax)
+        st.pyplot(fig)
--- tentang.py
+++ tentang.py
@@ -3,3 +3,5 @@ import streamlit as st
 def tampilkan_tentang():
     st.write('Nama saya Isna')
     st.write('Saat ini saya sedang belajar buat Portofolio Data')
+
+    st.image('new me.jpg', caption='Isna Ayustin')
